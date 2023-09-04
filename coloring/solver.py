4623005e168b30666d1c175beea294022cc8723e
Harsh Patel <harshpatel1692@gmail.com>
2023-09-04 13:09:23 -0500

    Scoring 60/60 using local search and tabu search

--- coloring/solver.py
+++ coloring/solver.py
@@ -2,68 +2,194 @@
 # -*- coding: utf-8 -*-
 from tqdm import tqdm
 import sys
+import random
+from collections import Counter
+from copy import deepcopy
+
+
+class TabuList:
+    def __init__(self, tabu_size):
+        self.tabu_size = tabu_size
+        self.tabu_hash = set()
+        self.tabu_queue = []
+
+    def is_present(self, node):
+        return node in self.tabu_hash
+
+    def insert(self, node):
+        if self.is_present(node):
+            return
+        self.tabu_hash.add(node)
+        self.tabu_queue.append(node)
+        if len(self.tabu_hash) > self.tabu_size:
+            self.remove()
+
+    def remove(self):
+        top = self.tabu_queue.pop(0)
+        self.tabu_hash.remove(top)
+
+
+def get_conflicts(graph, coloring):
+
+    node_count = len(graph)
+    conflicts = [0 for _ in range(node_count)]
+
+    for node, neighbors in enumerate(graph):
+        for neighbor in neighbors:
+            if coloring[node] == coloring[neighbor]:
+                conflicts[node] += 1
+
+    return conflicts
+
+
+def max_violation_node(conflict_mapping, tabu_list):
+
+    max_conflicts = max([conflict for node, conflict in enumerate(conflict_mapping) if not tabu_list.is_present(node)])
+
+    if max_conflicts == 0:
+        return -1
+
+    # Create a new dictionary with only the key-value pairs that have the maximum value and not in tabu list
+    max_conflict_nodes = [node for node, conflict in enumerate(conflict_mapping) if conflict == max_conflicts and not tabu_list.is_present(node)]
+
+    if not max_conflict_nodes:
+        return -1
+
+    return random.choice(max_conflict_nodes)
+
+
+def remove_color(coloring, max_colors):
+
+    pick_color = random.randint(0, max_colors-1)
+
+    new_coloring = []
+
+    for c in coloring:
+        if c == pick_color:
+            new_coloring.append(random.randint(0, max_colors-2))
+        elif c > pick_color:
+            new_coloring.append(c-1)
+        else:
+            new_coloring.append(c)
+
+    return new_coloring
+
+
+def unfamous_color(node, graph, coloring, max_colors):
+
+    node_color = coloring[node]
+    neighbor_colors = [coloring[color] for color in graph[node]]
+    color_count = Counter(neighbor_colors)
+
+    min_color = min([v for k, v in color_count.items() if k != node_color])
+
+    unused_colors = list(set(range(max_colors))-set(color_count.keys()))
+
+    if unused_colors:
+        return random.choice(unused_colors)
+    else:
+        least_colors = [k for k, v in color_count.items() if v == min_color and k != node_color]
+        return random.choice(least_colors)
+
+
+def check_feasibility(graph, coloring, max_colors, tabu_limit):
+    max_iterations = 50000
+    iteration = 0
+    conflicts = get_conflicts(graph, coloring)
+    total_conflicts = sum(conflicts)
+    tabu_list = TabuList(tabu_limit)
+
+    while iteration < max_iterations and total_conflicts > 0:
+
+        node = max_violation_node(conflicts, tabu_list)
+
+        while node == -1:
+            tabu_list.remove()
+            node = max_violation_node(conflicts, tabu_list)
+
+        tabu_list.insert(node)
+        least_used_color = unfamous_color(node, graph, coloring, max_colors)
+
+        for neighbor in graph[node]:
+            if coloring[neighbor] == coloring[node]:
+                conflicts[neighbor] -= 1
+                conflicts[node] -= 1
+            elif coloring[neighbor] == least_used_color:
+                conflicts[neighbor] += 1
+                conflicts[node] += 1
+
+        coloring[node] = least_used_color
+
+        # conflicts = get_conflicts(graph, coloring)
+        total_conflicts = sum(conflicts)
+
+        iteration += 1
+
+    return sum(conflicts) == 0, coloring, conflicts, iteration
+
+
+def get_suboptimal_coloring(mapping):
+
+    mapping = deepcopy(mapping)
+
+    node_count = len(mapping)
+    coloring = [[] for _ in range(node_count)]
+
+    for idx in range(node_count):
+        coloring[idx] = list(range(node_count))
+
+    for start_node in range(node_count):
+
+        if isinstance(coloring[start_node], list):
+            coloring[start_node] = coloring[start_node][0]
+        else:
+            continue
+
+        for j in mapping[start_node]:
+
+            if coloring[start_node] in coloring[j]:
+                coloring[j].remove(coloring[start_node])
+
+            if start_node in mapping[j]:
+                mapping[j].remove(start_node)
+
+    del mapping
+    return coloring
 
 
 def logic(edges, node_count):
 
-    mapping = dict()
-    best_min_colors = node_count
-    optimal_colors = []
+    tabu_limit = node_count // 10
 
-    for idx in range(node_count):
-        mapping[idx] = []
+    mapping = [[] for _ in range(node_count)]
 
     for i, j in edges:
         mapping[i].append(j)
         mapping[j].append(i)
 
-    priority_of_nodes = list(dict(sorted(mapping.items(), key=lambda e: len(e[1]), reverse=True)).keys())[:10]
+    new_coloring = get_suboptimal_coloring(mapping) #[random.randint(0, max_color-1) for _ in range(node_count)]
+    max_color = len(set(new_coloring))
 
-    for start_node in tqdm(priority_of_nodes):
+    max_retries = tabu_limit
+    retry = max_retries
 
-        mapping = dict()
-        coloring = dict()
+    while retry > 0:
 
-        for idx in range(node_count):
-            coloring[idx] = list(range(node_count))
+        is_feasible, coloring, conflicts, iteration = check_feasibility(mapping, new_coloring, max_color, tabu_limit)
 
-        for idx in range(node_count):
-            mapping[idx] = []
+        if is_feasible:
+            max_color -= 1
+            optimal_coloring = deepcopy(coloring)
+            retry = max_retries
 
-        for i, j in edges:
-            mapping[i].append(j)
-            mapping[j].append(i)
+        else:
+            retry -= 1
 
-        mapping = dict(sorted(mapping.items(), key=lambda e: len(e[1]), reverse=True))
+        new_coloring = remove_color(optimal_coloring, max_color)
 
-        while len(mapping) > 0:
+    print(max_color, is_feasible, len(set(coloring)), sum(conflicts), iteration)
 
-            coloring[start_node] = coloring[start_node][0]
-            connected_nodes = mapping.pop(start_node)
-
-            for j in connected_nodes:
-
-                if coloring[start_node] in coloring[j]:
-                    coloring[j].remove(coloring[start_node])
-
-                if start_node in mapping[j]:
-                    mapping[j].remove(start_node)
-
-            if mapping:
-                mapping = dict(sorted(mapping.items(), key=lambda e: len(e[1]), reverse=True))
-                start_node = list(mapping.keys())[0]
-
-        validation_counter = 0
-        for i, j in edges:
-            status = False if coloring[i] != coloring[j] else True
-            if status:
-                validation_counter += 1
-
-        if validation_counter == 0 and len(set(coloring.values())) < best_min_colors:
-            best_min_colors = len(set(coloring.values()))
-            optimal_colors = coloring
-
-    return optimal_colors
+    return optimal_coloring
 
 
 def solve_it(input_data):
@@ -87,8 +213,8 @@ def solve_it(input_data):
     solution = logic(edges, node_count)
 
     # prepare the solution in the specified output format
-    output_data = str(len(set(solution.values()))) + ' ' + str(0) + '\n'
-    output_data += ' '.join(map(str, solution.values()))
+    output_data = str(len(set(solution))) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, solution))
 
     return output_data
 
